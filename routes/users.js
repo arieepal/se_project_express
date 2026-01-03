2349832a080cfc60c10586f5e9d140d70994a4f1
Arielle <arielle.palmer12@gmail.com>
2026-01-02 21:59:14 -0600

    remove validation

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const { getCurrentUser, updateProfile } = require("../controllers/users");
 const auth = require("../middleware/auth");
-const { validateUserBody } = require("../middleware/validation");
 
 router.get("/me", auth, getCurrentUser);
 
-router.patch("/me", auth, validateUserBody, updateProfile);
+router.patch("/me", auth, updateProfile);
 
 module.exports = router;
